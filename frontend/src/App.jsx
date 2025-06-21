bc40778c77680832f1173479cf11a1e56e3da8e0
Hershyl <hershyls@missionreadyhq.com>
2025-06-21 14:09:56 +1200

    Footer Responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
     <>
       {/* Your other page content goes here */}
       <div className="min-h-screen">
-        <h1 className="text-h1 p-8">Welcome to Z Energy</h1>
         {/* More main content */}
       </div>
 
